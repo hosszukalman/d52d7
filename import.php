6c07d4ab3625b548fcaaf41a53b412b6bde12041
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-11-08 06:34:26 +0100

    Add autoloader function.

--- import.php
+++ import.php
@@ -5,12 +5,24 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 
+/**
+ * Autoloader function to load classes from lib folder.
+ *
+ * @param type $class_name
+ */
+function __autoload($class_name) {
+  include 'lib/' . $class_name . '.php';
+}
+
 try {
   // DB connections
   $dsn = 'mysql:dbname=epiteszforum_old;host=localhost';
   $user = 'epiteszforum';
   $password = 'ccMeLHhnTrSBbtrw';
   $dbhImport = new PDO($dsn, $user, $password);
+
+  // Store DB connections to Registry
+  Registry::set('dbhImport', $dbhImport);
 
   $dbhImport->exec('SET NAMES "UTF8"');
 
