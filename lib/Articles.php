5443b6242a12476ca9669f18bc49c91cfcd49e74
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-11-09 06:18:31 +0100

    Create base query for articles.

--- lib/Articles.php
+++ lib/Articles.php
@@ -14,6 +14,13 @@ class Articles extends Importer {
   }
 
   public function execute() {
+    foreach ($this->dbhImport->query("SELECT n.*, cft.field_trzs_value, cfb.field_bevezet_value, cfea.field_eng_abstract_value FROM node n
+      LEFT JOIN content_field_trzs cft USING(vid)
+      LEFT JOIN content_field_bevezet cfb USING(vid)
+      LEFT JOIN content_field_eng_abstract cfea USING(vid)
+      WHERE n.type = 'normal_tartalom' ORDER BY n.created", PDO::FETCH_ASSOC) as $oldContent) {
 
+
+    }
   }
 }
