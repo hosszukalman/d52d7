d53cf8062d8e32b6ae4087d2fa05f057284e86c1
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-11-09 05:49:31 +0100

    Add base of Articles class.

---
+++ lib/Articles.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Import terms
+ */
+class Articles extends Importer {
+
+  function __construct() {
+    parent::__construct();
+
+  }
+
+  public function deleteAll() {
+
+  }
+
+  public function execute() {
+
+  }
+}
