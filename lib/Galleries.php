5231785d02fbb217e11a55034bce6b79f63724d1
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-11-18 16:07:09 +0100

    Save gallery images

--- lib/Galleries.php
+++ lib/Galleries.php
@@ -2,8 +2,17 @@
 
 class Galleries extends Importer {
 
+  /**
+   * @var PDOStatement
+   */
+  private $getImages;
+
   function __construct() {
     parent::__construct();
+
+    $this->getImages = $this->dbhImport->prepare("SELECT igc.*, i.image FROM imagelist_gallery_cache igc
+      INNER JOIN imagelist i USING(img_id)
+      WHERE igc.nid = :gallery_nid");
   }
 
   public function deleteAll() {
@@ -22,6 +31,7 @@ class Galleries extends Importer {
 
     // Delete from connection DB.
     $this->dbhConnection->exec('TRUNCATE TABLE galleries');
+    $this->dbhConnection->exec('TRUNCATE TABLE gallery_images');
   }
 
   public function execute() {
@@ -31,7 +41,7 @@ class Galleries extends Importer {
   private function storeGalleryNodes() {
     $counter = 0;
     foreach ($this->dbhImport->query("SELECT n.* FROM imagelist_gallery_cache igc
-      INNER JOIN node n USING(nid) GROUP BY igc.nid", PDO::FETCH_ASSOC) as $gallery) {
+      INNER JOIN node n USING(nid) GROUP BY igc.nid ORDER BY igc.nid", PDO::FETCH_ASSOC) as $gallery) {
 
       // New node objec
       $node = new stdClass();
@@ -54,10 +64,36 @@ class Galleries extends Importer {
       $node->media_gallery_image_info_where[LANGUAGE_NONE][0]['value'] = 'nothing';
       $node->media_gallery_allow_download[LANGUAGE_NONE][0]['value'] = 1;
 
+      // Get images
+      $this->getImages->execute(array(':gallery_nid' => $gallery['nid']));
+      $images = $this->getImages->fetchAll(PDO::FETCH_ASSOC);
+
+      // Store images to conenction DB
+      $store_images = array();
+
+      foreach ($images as $image) {
+//        $file = media_parse_to_file('sites/default/' . $gallery['image']);
+        $file = media_parse_to_file('sites/default/files/tmpimages/' . rand(1, 16) . '.jpeg');
+        $node->media_gallery_media[LANGUAGE_NONE][] = array(
+          'fid' => $file->fid,
+          'title' => $image['title'],
+          'data' => '',
+        );
+        $store_images[] = array(
+          'old_image_id' => $image['img_id'],
+          'fid' => $file->fid,
+        );
+      }
 
       node_save($node);
 
+      // Store node
       $this->dbhConnection->query("INSERT INTO galleries VALUES ({$gallery['nid']}, {$node->nid})");
+
+      // Sore images
+      foreach ($store_images as $stored) {
+        $this->dbhConnection->query("INSERT INTO gallery_images VALUES ({$stored['old_image_id']}, {$stored['fid']}, {$node->nid})");
+      }
 
       echo $counter++ . PHP_EOL;
 
@@ -66,6 +102,5 @@ class Galleries extends Importer {
       }
     }
   }
-
 
 }
