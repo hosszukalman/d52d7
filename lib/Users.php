329a17a4b266147e1f07e91ba3f85d8f8903b7e4
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-11-23 06:57:26 +0100

    Users base data save

--- lib/Users.php
+++ lib/Users.php
@@ -14,7 +14,20 @@ class Users extends Importer {
     $counter = 0;
     foreach ($this->dbhImport->query("SELECT u.* FROM users u WHERE u.uid NOT IN (0, 1) ORDER BY u.uid", PDO::FETCH_ASSOC) as $oldUser) {
 
-      
+      // User base informations
+      $user = new stdClass();
+      $user->name = $oldUser['name'];
+      $user->mail = $oldUser['mail'];
+      $user->signature = $oldUser['signature'];
+      $user->signature_format = 'filtered_html';
+      $user->created = $oldUser['created'];
+      $user->access = $oldUser['access'];
+      $user->login = $oldUser['login'];
+      $user->status = $oldUser['status'];
+
+      $newAccount = user_save($user, array());
+
+      $this->dbhConnection->query("INSERT INTO user VALUES ({$oldUser['uid']}, {$newAccount->uid})");
 
       echo $counter++ . PHP_EOL;
 
