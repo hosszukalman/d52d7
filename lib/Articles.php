2196822a452071c7746ea8b8892771e7ca8d73d7
Kalman Hosszu <hosszu.kalman@gmail.com>
2012-01-03 19:16:31 +0100

    Remove node function from article class

--- lib/Articles.php
+++ lib/Articles.php
@@ -6,18 +6,6 @@
  */
 class Articles extends Nodes {
 
-  private $termMap;
-
-  /**
-   * @var PDOStatement
-   */
-  private $getNewTerms;
-
-  /**
-   * @var PDOStatement
-   */
-  private $getOldTerms;
-
   /**
    * @var PDOStatement
    */
@@ -33,36 +21,16 @@ class Articles extends Nodes {
    */
   private $getImageData;
 
-  /**
-   * @var PDOStatement
-   */
-  private $getAttachemtns;
-
   function __construct() {
     parent::__construct();
 
-    $this->getNewTerms = $this->dbhConnection->prepare("SELECT t.new_tid FROM terms t WHERE t.old_tid = :old_tid");
+    $this->prepareAttachmentQuery();
+
     $this->getRelatedGallery = $this->dbhConnection->prepare("SELECT g.gallery_nid FROM galleries g WHERE g.old_nid = :old_nid");
     $this->getNewImage = $this->dbhConnection->prepare("SELECT gi.* FROM gallery_images gi WHERE gi.old_image_id = :old_image_id");
     $this->getImageData = $this->dbhImport->prepare("SELECT i.* FROM imagelist i WHERE img_id = :img_id");
-    $this->getAttachemtns = $this->dbhImport->prepare("SELECT f.* FROM files f WHERE f.nid = :old_nid");
-    $this->getOldTerms = $this->dbhImport->prepare("SELECT td.* FROM term_node tn
-      INNER JOIN term_data td USING(tid)
-      WHERE tn.nid = :nid AND td.vid IN (13, 2, 8, 7, 14)");
 
-    foreach ($this->dbhImport->query("SELECT td.* FROM node n
-      RIGHT JOIN term_node tn USING(nid)
-      RIGHT JOIN term_data td ON (tn.tid = td.tid)
-      WHERE n.type = 'normal_tartalom' AND td.vid IN (13, 2, 8, 7, 14)
-      GROUP BY td.tid ORDER BY n.created", PDO::FETCH_ASSOC) as $term) {
-
-      $this->getNewTerms->execute(array(':old_tid' => $term['tid']));
-      $result = $this->getNewTerms->fetch(PDO::FETCH_ASSOC);
-      if (!empty($result)) {
-//        $this->termMap[$term['vid']][$term['tid']] = $result['new_tid'];
-        $this->termMap[$term['tid']] = $result['new_tid'];
-      }
-    }
+    $this->prepareTerms('normal_tartalom');
   }
 
   public function deleteAll() {
@@ -197,55 +165,5 @@ class Articles extends Nodes {
 //        exit;
 //      }
     }
-  }
-
-  protected function galleryFilter($text) {
-    // Find and replace arrays.
-    $search = array();
-    $replace = array();
-
-    // Regular expression
-    $finds = preg_match_all('/\[imagelist\|imgid=([0-9]*)[^\]]*\]/', $text, $matches);
-
-    if ($finds) {
-      foreach ($matches[0] as $id => $fullPattern) {
-        $newPattern = '';
-
-        // Select the the image data
-        $this->getNewImage->execute(array(':old_image_id' => $matches[1][$id]));
-        $newImage = $this->getNewImage->fetch(PDO::FETCH_ASSOC);
-
-        if ($newImage) {
-          $newPattern = "[mg_picker:{$newImage['gallery_nid']} fid:{$newImage['fid']}]";
-        }
-
-        // Replace with data (clear if the old image is not exist in the new DB)
-        $search[] = $fullPattern;
-        $replace[] = $newPattern;
-      }
-    }
-
-    // Replace the finded patterns with the links
-    return str_replace($search, $replace, $text);
-  }
-
-  protected function mainImage(&$text) {
-    // Regular expression
-    $finds = preg_match('/\[imagelist\|imgid=([0-9]*)[^\]]*\]/', $text, $matches);
-
-    if ($finds) {
-      $text = str_replace($matches[0], '', $text);
-
-      // Select the the image data
-      $this->getImageData->execute(array(':img_id' => $matches[1]));
-      $imageData = $this->getImageData->fetch(PDO::FETCH_ASSOC);
-
-      if ($imageData) {
-        return 'sites/default/' . $imageData['image'];
-//        return 'sites/default/files/tmpimages/' . rand(1, 16) . '.jpeg';
-      }
-    }
-
-    return FALSE;
   }
 }
